8d1bad967b6c839b24674f024b99349e5c076f32
eight <eight04@gmail.com>
2016-02-25 20:56:44 +0800

    Fix relative path issue

--- link_to_plugin_folder.bat
+++ link_to_plugin_folder.bat
@@ -1,2 +1,8 @@
+REM Run me under administrative privileges
+pushd %~dp0
+
 mklink "C:\Program Files (x86)\Notepad++\plugins\jN\includes\emmet.js" "dist\emmet.js"
 mklink /D "C:\Program Files (x86)\Notepad++\plugins\jN\includes\emmet\" "dist\emmet"
+mklink /D "C:\Program Files (x86)\Notepad++\plugins\jN\includes\dialog\" "dist\dialog"
+
+popd
