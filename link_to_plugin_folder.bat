18a2a8e3d7f9861da577041a4d1f399af5450eee
eight <eight04@gmail.com>
2016-02-26 02:02:33 +0800

    Fix relative path problem

--- link_to_plugin_folder.bat
+++ link_to_plugin_folder.bat
@@ -1,9 +1,6 @@
 REM Run me under administrative privileges
-pushd %~dp0
-
-mklink "C:\Program Files (x86)\Notepad++\plugins\jN\includes\emmet.js" "dist\emmet.js"
-mklink /D "C:\Program Files (x86)\Notepad++\plugins\jN\includes\emmet\" "dist\emmet"
-mklink /D "C:\Program Files (x86)\Notepad++\plugins\jN\includes\dialog\" "dist\dialog"
-mklink /D "C:\Program Files (x86)\Notepad++\plugins\jN\includes\FileStream\" "dist\FileStream\"
-
-popd
+mklink "C:\Program Files (x86)\Notepad++\plugins\jN\includes\emmet.js" "%~dp0dist\emmet.js"
+mklink /D "C:\Program Files (x86)\Notepad++\plugins\jN\includes\emmet\" "%~dp0dist\emmet"
+mklink /D "C:\Program Files (x86)\Notepad++\plugins\jN\includes\dialog\" "%~dp0dist\dialog"
+mklink /D "C:\Program Files (x86)\Notepad++\plugins\jN\includes\FileStream\" "%~dp0dist\FileStream"
+pause
