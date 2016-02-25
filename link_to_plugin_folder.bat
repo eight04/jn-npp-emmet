9fd208657505bf341fc75ef9d9b325b03733305c
eight <eight04@gmail.com>
2016-02-26 01:57:47 +0800

    Update symlink

--- link_to_plugin_folder.bat
+++ link_to_plugin_folder.bat
@@ -4,5 +4,6 @@ pushd %~dp0
 mklink "C:\Program Files (x86)\Notepad++\plugins\jN\includes\emmet.js" "dist\emmet.js"
 mklink /D "C:\Program Files (x86)\Notepad++\plugins\jN\includes\emmet\" "dist\emmet"
 mklink /D "C:\Program Files (x86)\Notepad++\plugins\jN\includes\dialog\" "dist\dialog"
+mklink /D "C:\Program Files (x86)\Notepad++\plugins\jN\includes\FileStream\" "dist\FileStream\"
 
 popd
