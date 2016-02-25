22129d7e663bc427595a879717069ab928454e9a
eight <eight04@gmail.com>
2016-02-25 08:45:31 +0800

    Use relative path

--- link_to_plugin_folder.bat
+++ link_to_plugin_folder.bat
@@ -1,2 +1,2 @@
 mklink "C:\Program Files (x86)\Notepad++\plugins\jN\includes\emmet.js" "dist\emmet.js"
-mklink /D "C:\Program Files (x86)\Notepad++\plugins\jN\includes\emmet\" "D:\Dev\jn-npp-emmet\dist\emmet"
+mklink /D "C:\Program Files (x86)\Notepad++\plugins\jN\includes\emmet\" "dist\emmet"
