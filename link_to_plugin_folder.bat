e8bac7dc61fbc9fc98f32fd1057d434ba603e4ef
eight <eight04@gmail.com>
2018-03-10 11:18:14 +0800

    Fix: remove unused path

--- link_to_plugin_folder.bat
+++ link_to_plugin_folder.bat
@@ -1,6 +1,4 @@
 REM Run me under administrative privileges
 mklink "C:\Program Files (x86)\Notepad++\plugins\jN\includes\emmet.js" "%~dp0dist\emmet.js"
 mklink /D "C:\Program Files (x86)\Notepad++\plugins\jN\includes\emmet\" "%~dp0dist\emmet"
-mklink /D "C:\Program Files (x86)\Notepad++\plugins\jN\includes\dialog\" "%~dp0dist\dialog"
-mklink /D "C:\Program Files (x86)\Notepad++\plugins\jN\includes\FileStream\" "%~dp0dist\FileStream"
 pause
